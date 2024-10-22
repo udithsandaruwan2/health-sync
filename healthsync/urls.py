39fec1ad2318f2f67ca102b006312e7bf86118bd
Udith Sandaruwan <developer.udithsandaruwan@gmail.com>
2024-10-22 23:40:52 +0530

    phrase 23

--- healthsync/urls.py
+++ healthsync/urls.py
@@ -2,15 +2,28 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('', include('users.urls')),
     path('captcha/', include('captcha.urls')),
+    
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), 
+        name='password-reset'),
+    
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), 
+        name='password_reset_done'),
+    
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), 
+        name='password_reset_confirm'), 
+    
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'),
+    
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
